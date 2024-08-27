bbf8440e0ca9e03c3a8094ecae23d277f361ec27
Dmitry Sorokin (@sorydima) <43252008+sorydima@users.noreply.github.com>
2024-08-27 20:21:59 +0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/porosenocheck/employee/MainActivity.kt
+++ android/app/src/main/kotlin/com/porosenocheck/employee/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.porosenocheck.employee
+package com.porosenocheck.employeefor
 
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterFragmentActivity
