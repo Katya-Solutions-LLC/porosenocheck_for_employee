2886375e4b91420417ed71b1bc5f6d8842345c90
Dmitry Olegovich Sorokin (@sorydima) <43252008+sorydima@users.noreply.github.com>
2025-04-15 21:50:30 +0300

    Add files via upload

---
+++ aurora/flutter/generated_plugin_registrant.cpp
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+#include <flutter/flutter_aurora.h>
+
+#include "generated_plugin_registrant.h"
+
+namespace aurora {
+void RegisterPlugins() {
+  flutter::PluginRegistrar* registrar = GetPluginRegistrar();
+}
+}
